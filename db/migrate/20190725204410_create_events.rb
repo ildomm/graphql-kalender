0f5e2906daa00d6739c318b3a041991c7114e5cc
ildo.grings <ildo.grings@stratum.bt>
2019-07-25 21:55:54 -0300

    Migrate Sources and Events

---
+++ db/migrate/20190725204410_create_events.rb
@@ -0,0 +1,24 @@
+class CreateEvents < ActiveRecord::Migration[5.1]
+  def change
+
+    create_table :sources do |t|
+      t.string :name, null: false
+      t.string :url, null: false
+      t.index :url, unique: true
+      t.timestamps
+    end
+
+    create_table :events do |t|
+      t.references :source, null: false, foreign_key: true
+      t.datetime :start_at, null: false
+      t.datetime :end_at, null: false
+      t.string :title, null: false
+      t.string :url, null: false
+      t.index :start_at, unique: false
+      t.index :end_at, unique: false
+      t.index :title, unique: false
+      t.timestamps
+    end
+
+  end
+end
